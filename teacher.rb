72a31534c6da38307460b7ce88ac5cafb861aa19
cherelemma <chere.lemma@aastu.edu.et>
2023-05-22 18:35:25 +0300

    add super to Teacher class to extends its parent's constructor

--- teacher.rb
+++ teacher.rb
@@ -1,7 +1,8 @@
 require_relative './person'
 
 class Teacher < Person
-  def initailize(specialization)
+  def initialize(specialization, age, name = 'Unknown', parent_permission: true)
+    super(name, age, parent_permission)
     @specialization = specialization
   end
 
