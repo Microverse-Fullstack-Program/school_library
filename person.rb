61705e9fe6d3b7d88c57e0e1f92d8c3c3608f3a5
cherelemma <chere.lemma@aastu.edu.et>
2023-05-22 17:36:25 +0300

    update setter and getter methods with shorthands

--- person.rb
+++ person.rb
@@ -1,35 +1,12 @@
 class Person
+  attr_accessor :name, :age # getter & setter method
+  attr_reader :age # getter method
+
   def initialize (age, name = 'Unknown', parent_permission = true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
-  end
-
-  # getter methods
-  def id
-    @id
-  end
-
-  def name
-    @name
-  end
-
-  def age
-    @age
-  end
-
-  # setter methods
-  def id=(id)
-    @id=id
-  end
-
-  def name=(name)
-    @name = name
-  end
-
-  def age=(age)
-    @age = age
   end
 
   # private method
