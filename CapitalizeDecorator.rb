aaf66f175409a872427abe42b201144962327b48
cherelemma <chere.lemma@aastu.edu.et>
2023-05-23 14:50:31 +0300

    create CapitalizeDecorator class

---
+++ CapitalizeDecorator.rb
@@ -0,0 +1,12 @@
+require_relative './BaseDecorator'
+
+class CapitalizeDecorator < BaseDecorator
+
+  def initialize (nameable)
+    @nameable = nameable
+  end
+  
+  def correct_name
+    @nameable.correct_name.upcase
+  end
+end
