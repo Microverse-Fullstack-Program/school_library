fbeb0aaae1f0c6a886310fefc5028a22d45b214f
cherelemma <chere.lemma@aastu.edu.et>
2023-05-23 15:24:27 +0300

    fix linter errors

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -1,10 +1,6 @@
 require_relative './base_decorator'
 
 class TrimmerDecorator < BaseDecorator
-  def initialize(nameable)
-    super(nameable)
-  end
-
   def correct_name
     length = @nameable.correct_name.length
     return @nameable.correct_name[0...10] if length > 10
