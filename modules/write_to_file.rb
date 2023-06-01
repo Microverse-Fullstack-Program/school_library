174b0c8fad6c61f3c6c231e677c1c579837f3d2d
cherelemma <chere.lemma@aastu.edu.et>
2023-06-01 22:54:47 +0300

    write persons record to JSON file

--- modules/write_to_file.rb
+++ modules/write_to_file.rb
@@ -12,4 +12,30 @@ module WriteToFile
     file.write(JSON.pretty_generate(book_hash))
     file.close
   end
+
+  # write persons record to JSON file
+  def write_person(persons)
+    file = File.open('./data/person.json', 'w+')
+
+    person_hash = {}
+    person_hash['students'] = {}
+    person_hash['teachers'] = {}
+
+    persons.each_with_index do |person, index|
+      if person.instance_of?(Student)
+        person_hash['students'][(index + 1).to_s] = {
+          'id' => person.id, 'name' => person.name, 'age' => person.age,
+          'parent_permission' => person.parent_permission, 'classroom' => person.classroom
+        }
+      elsif person.instance_of?(Teacher)
+        person_hash['teachers'][(index + 1).to_s] = {
+          'id' => person.id, 'name' => person.name, 'age' => person.age,
+          'parent_permission' => person.parent_permission, 'specialization' => person.specialization
+        }
+      end
+    end
+
+    file.write(JSON.pretty_generate(person_hash))
+    file.close
+  end
 end
