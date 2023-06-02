b206c57696956a9b5aab81751e2e38ef5d67eece
Odongo006 <www.henryodongo006@gmail.com>
2023-06-02 11:33:36 +0300

    Add read book class

--- modules/read_from_file.rb
+++ modules/read_from_file.rb
@@ -4,4 +4,16 @@ require_relative '../student'
 require_relative '../rental'
 
 module ReadFromFile
+    #read book from file
+    def read_book(books)
+        file = File.open('./data/book.json', 'r') if File.exist?('./data/book.json')
+        return false if file.nil?
+    
+        return unless file.size.positive?
+    
+        books_record = JSON.parse(file.read)
+        books_record.each do |_key, book|
+          books << Book.new(book['title'], book['author'])
+        end
+      end
 end
